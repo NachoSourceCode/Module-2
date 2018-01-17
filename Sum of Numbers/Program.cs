cfaaebcba4a34f6d418bbf81c4cce6334549f363
Matthew Slone <slonem2@my.erau.edu>
2018-01-17 18:51:36 -0500

    initial commit.

--- Sum of Numbers/Program.cs
+++ Sum of Numbers/Program.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace Sum_of_Numbers
 {
+    //initial commit.
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
