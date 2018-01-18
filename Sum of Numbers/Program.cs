00876af6ad01d9a67486ce362f6054684a869b86
Matthew Slone <slonem2@my.erau.edu>
2018-01-17 21:51:45 -0500

    Prompt user to enter a number, int.Parse will take that string representation of the value entered and convert it to the signed 32-bit equivialent, which is assigned to each variable. Then the sum of the variables entered will then be assigned to 'sum'. Finally the interpolated string is printed to the console window. The program exits after user enters any key.

--- Sum of Numbers/Program.cs
+++ Sum of Numbers/Program.cs
@@ -11,7 +11,26 @@ namespace Sum_of_Numbers
     {
         static void Main(string[] args)
         {
-            
+            /* Asks user to enter a number-->each number entered is the string representation of the value
+             and is converted to it's 32-bit signed integer equivalent; */
+            Console.WriteLine("Please enter a number:");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+            int num5 = int.Parse(Console.ReadLine());
+            int num6 = int.Parse(Console.ReadLine());
+            int num7 = int.Parse(Console.ReadLine());
+            int num8 = int.Parse(Console.ReadLine());
+            int num9 = int.Parse(Console.ReadLine());
+            int num10 = int.Parse(Console.ReadLine());
+
+            /* sets the variable 'sum' is equal to the sum of each of the variables */
+            int sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+
+            /* This statement prints the interpolated string which means the value will replace the 'sum'
+             variable. */
+            Console.WriteLine($"The average is: {sum}");
         }
     }
 }
