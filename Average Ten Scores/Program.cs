19312cb946f67283162113f36ba4d3849716812e
Matthew Slone <slonem2@my.erau.edu>
2018-01-17 22:04:02 -0500

    initialized 'average' and changed the last statement on line 34 to print the value of average(interpolated string).

---
+++ Average Ten Scores/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Average_Ten_Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Asks user to enter a number-->each number entered is the string representation of the value
+             and is converted to it's 32-bit signed integer equivalent; */
+            Console.WriteLine("Please enter a number:");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+            int num5 = int.Parse(Console.ReadLine());
+            int num6 = int.Parse(Console.ReadLine());
+            int num7 = int.Parse(Console.ReadLine());
+            int num8 = int.Parse(Console.ReadLine());
+            int num9 = int.Parse(Console.ReadLine());
+            int num10 = int.Parse(Console.ReadLine());
+
+            /* sets the variable 'sum' is equal to the sum of each of the variables. Then 'average' is set 
+              equal to 'sum' / 10.*/
+            int sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+            int average = sum / 10;
+
+            /* This statement prints the interpolated string which means the value will replace the 'average'
+             variable with it's value. */
+            Console.WriteLine($"The average is: {average}");
+        }
+    }
+}
