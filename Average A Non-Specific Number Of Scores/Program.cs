b6c52a77545bf580592cfbcdaf2d645d639e4d45
Matthew Slone <slonem2@my.erau.edu>
2018-01-22 16:49:25 -0500

    convert the numerical average of test scores entered and return the letterGrade that is equivalent.

--- Average A Non-Specific Number Of Scores/Program.cs
+++ Average A Non-Specific Number Of Scores/Program.cs
@@ -29,7 +29,7 @@ namespace Average_A_Specific_Number_Of_Scores
            var letterGrade = makeLetterGrade(avg);
 
             // Output the grade
-           Console.WriteLine($"The average letter grade for this test is: {letterGrade}");
+           Console.WriteLine($"The average letter grade for these tests are: {letterGrade}");
         }
         // Converts the string representation of the number entered and converts it to it's integer 
         // representation and passes that value back to nTests. 
@@ -43,7 +43,6 @@ namespace Average_A_Specific_Number_Of_Scores
                 //if score entered are between 0 and 100 do something.
         static int inputTestScores(int nTests)
         {
-
             int sumOfScores = 0;
             var testKeeperTrackerOfer = 0; 
             // Do this while number of tests to enter hasn't been met.
@@ -51,16 +50,16 @@ namespace Average_A_Specific_Number_Of_Scores
             // keep track of current tests that are valid.
             while (testKeeperTrackerOfer < nTests)
             {
-                //create query of test score.
+                // create query of test score.
                 Console.WriteLine("Enter a test score:");
                 int testScore = int.Parse(Console.ReadLine());
 
-                //test score entered to be between 0 and 100.
+                // test score entered to be between 0 and 100.
                 if (testScore >= 0 && testScore <= 100)
                 {
                     // do something
                     sumOfScores += testScore;
-                    //record number of times the score has been entered.
+                    // record number of times the score has been entered.
                     testKeeperTrackerOfer += 1;
                 }
                 else
@@ -73,8 +72,40 @@ namespace Average_A_Specific_Number_Of_Scores
         }
           static char makeLetterGrade(int avg)
           {
-
+            
+            int letterGradeAvg = avg;
+            char letterGrade;
+            
+            // State what the range of A, B, C, D, and F.
+            if ((letterGradeAvg>= 90) && (letterGradeAvg<= 100))
+            {
+                letterGrade = 'A';
+            }
+            else if ((letterGradeAvg>= 80) && (letterGradeAvg<= 89))
+            {
+                letterGrade = 'B';
+            }
+            else if ((letterGradeAvg>= 70) && (letterGradeAvg<= 79))
+            {
+                letterGrade = 'C';
+            }
+            else if ((letterGradeAvg >= 60) && (letterGradeAvg <= 69))
+            {
+                letterGrade = 'D';
+            }
+            else if ((letterGradeAvg >= 0) && (letterGradeAvg <= 59))
+            {
+                letterGrade = 'F';
+            }
+            else
+            {
+               Console.WriteLine("You either scored so badly that there isn't a letter" +
+                    "grade for it or something else.");
+               letterGrade = 'Z';
+            }
+            // return Letter equivalent of numerical average.
+            //add throw statement.
+            return letterGrade;
           }
-          
     }
 }
